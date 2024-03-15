f5023cbfa9903fd2920180644f6f127794da0545
LedicaCo <webdesign@ledica.co>
2024-03-14 21:14:03 -0500

    Actualización repositorio

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -5,19 +5,94 @@ const Nosotros = () => {
     <>
       <div className="nosotros section" id="nosotros">
         <h2>Nosotros</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex voluptatibus quos rem ducimus ullam sed. Voluptates facilis explicabo dolores quam quibusdam quidem at omnis temporibus, aliquid consequuntur possimus libero laborum ex, nihil natus modi repudiandae ab veniam laudantium eum sed assumenda. Tempore aut molestiae blanditiis. Ratione, libero consequuntur. Doloremque voluptas dolore consectetur aperiam quia, quis quas mollitia omnis blanditiis alias non esse tenetur qui necessitatibus deleniti maxime commodi quidem? Nisi, adipisci aspernatur? Odio iusto illo mollitia officia delectus similique, optio ipsa laudantium, laboriosam qui cumque perferendis numquam repellat doloribus provident repudiandae corporis blanditiis maiores magni atque cum harum nobis! Dignissimos sequi animi, modi laboriosam deserunt, quisquam eum odio soluta dicta eligendi, cupiditate magni tenetur. Ratione amet obcaecati quae reiciendis, id libero ea dolores nostrum est, ad beatae distinctio vero aperiam delectus, consequuntur provident! Deleniti quaerat laboriosam provident nulla veritatis cumque eligendi, consequatur beatae ea cum ex aperiam omnis, non aliquam libero dolorem, facere adipisci doloribus. Veniam distinctio qui, totam, eum quasi ratione molestias perferendis aut voluptate sunt error.
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+          voluptatibus quos rem ducimus ullam sed. Voluptates facilis explicabo
+          dolores quam quibusdam quidem at omnis temporibus, aliquid
+          consequuntur possimus libero laborum ex, nihil natus modi repudiandae
+          ab veniam laudantium eum sed assumenda. Tempore aut molestiae
+          blanditiis. Ratione, libero consequuntur. Doloremque voluptas dolore
+          consectetur aperiam quia, quis quas mollitia omnis blanditiis alias
+          non esse tenetur qui necessitatibus deleniti maxime commodi quidem?
+          Nisi, adipisci aspernatur? Odio iusto illo mollitia officia delectus
+          similique, optio ipsa laudantium, laboriosam qui cumque perferendis
+          numquam repellat doloribus provident repudiandae corporis blanditiis
+          maiores magni atque cum harum nobis! Dignissimos sequi animi, modi
+          laboriosam deserunt, quisquam eum odio soluta dicta eligendi,
+          cupiditate magni tenetur. Ratione amet obcaecati quae reiciendis, id
+          libero ea dolores nostrum est, ad beatae distinctio vero aperiam
+          delectus, consequuntur provident! Deleniti quaerat laboriosam
+          provident nulla veritatis cumque eligendi, consequatur beatae ea cum
+          ex aperiam omnis, non aliquam libero dolorem, facere adipisci
+          doloribus. Veniam distinctio qui, totam, eum quasi ratione molestias
+          perferendis aut voluptate sunt error.
         </p>
-        <div className="mision">
-        <h2>Misión</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, incidunt atque nostrum repudiandae nobis corporis ipsam autem voluptatem porro dolore ipsum consequuntur eaque cumque vero similique, totam libero temporibus suscipit repellendus! Exercitationem voluptate veniam sunt quidem sapiente? Consequatur dolor nulla et delectus minus consectetur doloremque molestiae quos molestias error. Porro cum officiis repellat amet, dolore nesciunt dolor animi itaque reiciendis molestiae. Consectetur omnis quidem sit quos placeat corporis consequatur non molestiae sequi ducimus. Quisquam qui officia, sunt odio doloremque consequuntur quos modi molestias, sequi voluptates mollitia iusto reprehenderit cupiditate praesentium architecto! Voluptate?</p>
+        <div className="card-us">
+        <div className="card-title">
+                <h2>Mision</h2>
+              </div>
+          <div className="row-us">
+            <div className="img-us">
+              <img src="/mision.png" className="card-img" />
+            </div>
+            <div className="card-body">
+              
+              <p className="card-text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+                  voluptatibus quos rem ducimus ullam sed. Voluptates facilis
+                  explicabo dolores quam quibusdam quidem at omnis temporibus,
+                  aliquid consequuntur possimus libero laborum ex, nihil natus
+                  modi repudiandae ab veniam laudantium eum sed assumenda.
+                  Tempore aut molestiae blanditiis. Ratione, libero
+                  consequuntur. Doloremque voluptas dolore consectetur aperiam
+                  quia, quis quas mollitia omnis blanditiis alias non esse
+                  tenetur qui necessitatibus deleniti maxime commodi quidem?
+                  Nisi, adipisci aspernatur? Odio iusto illo mollitia officia
+                  delectus similique, optio ipsa laudantium, laboriosam qui
+                  cumque perferendis numquam repellat doloribus provident
+                  repudiandae corporis blanditiis maiores magni atque cum harum
+                  nobis! Dignissimos sequi animi, modi laboriosam deserunt,
+                  quisquam eum odio soluta dicta eligendi, cupiditate magni
+                  tenetur. Ratione amet obcaecati quae reiciendis, id libero ea
+                  dolores nostrum est, ad beatae distinctio vero aperiam.
+                </p>
+            </div>
+          </div>
         </div>
+
         <div className="vision">
-        <h2>Visión</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, incidunt atque nostrum repudiandae nobis corporis ipsam autem voluptatem porro dolore ipsum consequuntur eaque cumque vero similique, totam libero temporibus suscipit repellendus! Exercitationem voluptate veniam sunt quidem sapiente? Consequatur dolor nulla et delectus minus consectetur doloremque molestiae quos molestias error. Porro cum officiis repellat amet, dolore nesciunt dolor animi itaque reiciendis molestiae. Consectetur omnis quidem sit quos placeat corporis consequatur non molestiae sequi ducimus. Quisquam qui officia, sunt odio doloremque consequuntur quos modi molestias, sequi voluptates mollitia iusto reprehenderit cupiditate praesentium architecto! Voluptate?</p>
+          <h2>Visión</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+            incidunt atque nostrum repudiandae nobis corporis ipsam autem
+            voluptatem porro dolore ipsum consequuntur eaque cumque vero
+            similique, totam libero temporibus suscipit repellendus!
+            Exercitationem voluptate veniam sunt quidem sapiente? Consequatur
+            dolor nulla et delectus minus consectetur doloremque molestiae quos
+            molestias error. Porro cum officiis repellat amet, dolore nesciunt
+            dolor animi itaque reiciendis molestiae. Consectetur omnis quidem
+            sit quos placeat corporis consequatur non molestiae sequi ducimus.
+            Quisquam qui officia, sunt odio doloremque consequuntur quos modi
+            molestias, sequi voluptates mollitia iusto reprehenderit cupiditate
+            praesentium architecto! Voluptate?
+          </p>
         </div>
         <div className="valores">
-        <h2>Valores</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, incidunt atque nostrum repudiandae nobis corporis ipsam autem voluptatem porro dolore ipsum consequuntur eaque cumque vero similique, totam libero temporibus suscipit repellendus! Exercitationem voluptate veniam sunt quidem sapiente? Consequatur dolor nulla et delectus minus consectetur doloremque molestiae quos molestias error. Porro cum officiis repellat amet, dolore nesciunt dolor animi itaque reiciendis molestiae. Consectetur omnis quidem sit quos placeat corporis consequatur non molestiae sequi ducimus. Quisquam qui officia, sunt odio doloremque consequuntur quos modi molestias, sequi voluptates mollitia iusto reprehenderit cupiditate praesentium architecto! Voluptate?</p>
+          <h2>Valores</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+            incidunt atque nostrum repudiandae nobis corporis ipsam autem
+            voluptatem porro dolore ipsum consequuntur eaque cumque vero
+            similique, totam libero temporibus suscipit repellendus!
+            Exercitationem voluptate veniam sunt quidem sapiente? Consequatur
+            dolor nulla et delectus minus consectetur doloremque molestiae quos
+            molestias error. Porro cum officiis repellat amet, dolore nesciunt
+            dolor animi itaque reiciendis molestiae. Consectetur omnis quidem
+            sit quos placeat corporis consequatur non molestiae sequi ducimus.
+            Quisquam qui officia, sunt odio doloremque consequuntur quos modi
+            molestias, sequi voluptates mollitia iusto reprehenderit cupiditate
+            praesentium architecto! Voluptate?
+          </p>
         </div>
       </div>
     </>
