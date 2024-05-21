82956ace2fac65e2e839c38e2e21c25f5ef1d0b7
LedicaCo <webdesign@ledica.co>
2024-05-20 20:31:25 -0500

    Actualización repositorio

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -4,7 +4,7 @@ import '../assets/css/footer-copyright.css'
 const CopyRight = () => {
   return (
     <div className='footer-copyright'>
-    <span>Lagsi &copy; CopyRight - All right reserved <span className='separator'> &nbsp;-&nbsp; </span> <span className='nexo-creative'>Desarrollado por: <a className='footer-link' href="https://www.nexocreative.com.co">&nbsp;<img className='logo-powered' src="/logo-nexo-creative.png" />&nbsp; Nexo Creative S.A.S.</a></span></span>
+    <span>Lagsi &copy; CopyRight - All right reserved <span className='separator'> &nbsp;-&nbsp; </span> <span className='nexo-creative'>Desarrollado por: <a className='footer-link' href="https://www.nexocreative.com">&nbsp;<img className='logo-powered' src="/logo-nexo-creative.png" />&nbsp; Nexo Creative S.A.S.</a></span></span>
     </div>
   )
 }
